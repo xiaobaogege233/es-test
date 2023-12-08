322a3c932ae42f08b0e2a14906f626983040a7e8
bao <740824080@qq.com>
2023-12-08 10:40:53 +0800

    测试查询

--- src/main/java/cn/zchd/test/EsTest.java
+++ src/main/java/cn/zchd/test/EsTest.java
@@ -2,9 +2,12 @@ package cn.zchd.test;
 
 import cn.zchd.entity.HappinessrecordEntity;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch._types.mapping.Property;
 import co.elastic.clients.elasticsearch._types.query_dsl.*;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.indices.DeleteIndexResponse;
 import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
 import co.elastic.clients.json.JsonData;
@@ -19,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -36,7 +40,9 @@ public class EsTest {
 //        indexDetail(elasticsearchClient);
 //        createIndexAndMappings(elasticsearchClient);
 //        deleteIndex(elasticsearchClient);
-        indexIsExist(elasticsearchClient);
+//        indexIsExist(elasticsearchClient);
+//        getDocument(elasticsearchClient);
+        testMultipleCondition(elasticsearchClient);
     }
 
     // 创建索引并且映射字段
@@ -178,5 +184,73 @@ public class EsTest {
         }
 
         log.info("== {} 索引创建是否存在: {}", "elasticsearch-client", booleanResponse.value());
+    }
+
+
+
+    // 单挑查询  和数据库精准查询不一样 会有误差
+    public static void getDocument (ElasticsearchClient elasticsearchClient) {
+        SearchResponse<HappinessrecordEntity> search;
+        try {
+            search = elasticsearchClient.search(s -> s.index("happinessrecord")
+                            .from(0)  // 相当于mysql的offset
+                            .size(10) // 相当于mysql的size
+                            .query(q ->
+                                    q.term(t ->
+                                            t.field("realname").value(v -> v.stringValue("吴秀兰"))
+                                    )
+                            ),
+                    HappinessrecordEntity.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Hit<HappinessrecordEntity> hit: search.hits().hits()) {
+            log.info("== hit: source: {}, id: {}", hit.source(), hit.id());
+        }
+    }
+
+    // 多条件 返回查询 和数据库精准查询不一样 会有误差
+    public static void testMultipleCondition (ElasticsearchClient elasticsearchClient)  {
+
+        SearchRequest request = SearchRequest.of(searchRequest ->
+                searchRequest.index("happinessrecord")
+                        .from(0)
+                        .size(20)
+//                        .sort(s -> s.field(f -> f.field("age").order(SortOrder.Desc)))
+                        // 如果有多个 .query 后面的 query 会覆盖前面的 query
+                        .query(query ->
+                                query.bool(boolQuery ->
+                                        boolQuery
+                                                // 在同一个 boolQuery 中 must 会将 should 覆盖
+                                                .must(must -> must.term(
+//                                                        e -> e.field("age").gte(JsonData.of("21")).lte(JsonData.of("25"))
+                                                        e -> e.field("realname").value("吴秀兰")
+                                                ))
+//                                                .mustNot(mustNot -> mustNot.term(
+//                                                        e -> e.field("name").value(value -> value.stringValue("lisi1"))
+//                                                ))
+//                                                .should(must -> must.term(
+//                                                        e -> e.field("name").value(value -> value.stringValue("lisi2"))
+//                                                ))
+                                )
+                        )
+
+        );
+
+        SearchResponse<HappinessrecordEntity> searchResponse;
+        try {
+            searchResponse = elasticsearchClient.search(request, HappinessrecordEntity.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        log.info("返回的总条数有：{}", searchResponse.hits().total().value());
+        List<Hit<HappinessrecordEntity>> hitList = searchResponse.hits().hits();
+        for (Hit<HappinessrecordEntity> hit : hitList) {
+            log.info("== hit: {}, id: {}", hit.source(), hit.id());
+        }
+
     }
 }
